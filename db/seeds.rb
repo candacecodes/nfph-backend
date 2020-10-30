768f311efb446b24ed488981438365ea92b75781
candacecodes <candacehchoe@gmail.com>
2020-10-29 22:35:09 -0700

    some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,8 @@ Organization.create(name: "UCSF Benioff Children's Hospital", location: 'San Fra
 Organization.create(name: "University of Washington Medical Center - Montlake", location: 'Seattle, Washington', phone: '(206) 598-2282')
 Organization.create(name: "Seattle Children's Hospital", location: 'Seattle, Washington', phone: '(206) 987-2016')
 
-# Provider.create(first_name: 'firstname1', title: 'Title1', field: 'Field1', provider_uuid: '1', organization_id: 1)
+# Provider.create(first_name: 'firstname1', title: 'Title1', field: 'Field1', provider_uuid: '1', organization_id: 1, email_address: 'email1', password_digest: 'pw1', NPI_number: 123, last_name: 'lastname1')
+# rails c Provider.all not showing up
 
 
 Patient.create(first_name: 'firstname1', patient_uuid: '1', diagnosis: 'diagnosis1', prescriptions: ['prescription1'], organization_id: 1, email_address: 'email1', password_digest: 'password1', last_name: 'lastname1')
